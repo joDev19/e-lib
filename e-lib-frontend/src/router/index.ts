842237144a1ff66d79c1ccc47c88bfa4e58d7632
Jordy GNANIH <jordygnanihpro@gmail.com>
2026-09-22 10:50:16 +0200

    update file for ci/cd frontend

--- e-lib-frontend/src/router/index.ts
+++ e-lib-frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Livres from '@/views/Livres.vue'
+import Livres from '../views/Livres.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),

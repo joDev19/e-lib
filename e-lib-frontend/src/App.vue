c0673963954e5d57d51761527fc2ac5f623e5e71
Jordy GNANIH <jordygnanihpro@gmail.com>
2026-09-22 10:49:33 +0200

    Ajout de la pipeline ci/cd frontend

--- e-lib-frontend/src/App.vue
+++ e-lib-frontend/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import Livres from './views/Livres.vue';
 </script>
 
 <template>
   <header>
     <div class="h-screen bg-gray-100">
      
-      <Livres />
+      <RouterView /> 
+      
    
     </div>
   </header>
 
-  <!-- <RouterView /> -->
 </template>
 
 <style scoped>

7353491213cb5237fb6c00c6700a199c0bdf518f
Jordy GNANIH <jordygnanihpro@gmail.com>
2026-09-22 09:39:10 +0200

    add cors to backend

--- e-lib-backend/src/index.ts
+++ e-lib-backend/src/index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from "express";
 import pool from "./database/database";
 import router from "./routes/livres.routes"
+import cors from 'cors'
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.use("/e-lib/livres", router);
+// app.use("/e-lib/livres", router);
+app.use("/books", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Serveur Express TypeScript opérationnel");
